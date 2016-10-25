a61ec3940f1de4b746a12469519515b32e9672db
Desenvolvedores da Buscar [ID] <devbuscarid@gmail.com>
2016-10-24 22:55:22 -0200

    black friday araujo

---
+++ buscarid/black-friday-araujo-2016/index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Black Friday Araujo</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/media.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="texto col-md-6 col-md-offset-5">
+					Faça o seu cadastro <br/>
+					e receba em primeira mão <br/>
+					ofertas imperdíveis da Araujo!
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="ofertas col-md-12">
+					<form role="form">
+						<div class="col-md-3 form-group">
+							<label for="nome">
+								Nome
+							</label>
+							<input placeholder="Ex: João Augusto de Lima" type="email" class="form-control" id="nome" />
+						</div>
+						<div class="col-md-3 form-group">
+							<label for="email">
+								E-mail
+							</label>
+							<input placeholder="Ex: joaolima@gmail.com" type="email" class="form-control" id="email" />
+						</div>
+						<div class="col-md-2 form-group">
+							<label for="nascimento">
+								Data de nascimento
+							</label>
+							<input placeholder="dd/mm/aaaa" type="email" class="form-control" id="nascimento" />
+						</div>
+						<div class="col-md-2 form-group">
+							<label for="sexo">
+								Sexo
+							</label>
+							<select class="form-control selectpicker">
+							    <option>selecione</option>
+							    <option>Feminino</option>
+							    <option>Masculino</option>
+							</select>
+						</div>
+						<div class="col-md-2 form-group">
+							<button class="oferta btn btn-danger" type="submit">Receber Ofertas</button>
+						</div>
+					</form>
+					
+				</div>
+				<div class="col-md-6 col-md-offset-4">
+					<button class="botoes btn btn-primary" type="submit">
+						<i class="fa fa-facebook" aria-hidden="true"></i>
+	 					&nbsp; Registrar com Facebook
+ 					</button>
+					<button class="botoes btn btn-danger" type="submit">
+					<i class="fa fa-google-plus" aria-hidden="true"></i>
+						&nbsp; Registrar com Google
+					</button>
+				</div>
+			</div>
+		</div>
+	</header>
+
+
+	<section id="blackFriday">
+		<div class="container">
+			<div class="row2 bg-white borda">
+				<div class="col-md-offset-1 col-md-4">
+					<div class="box">
+					<img width="80%" src="assets/images/black-friday.jpg"/ alt="Black Friday">
+					<p>
+						A Black Friday, criada nos Estados Unidos da América, é um dia especial para o varejo. Isso porque nessa data específica o comércio coloca produtos à venda com grandes promoções e descontos imperdíveis. Esta data especial acontece todos os anos, sempre na última sexta-feira do mês de novembro. 
+					<p>
+					</div>
+				</div>
+				<div class="col-md-4 col-md-offset-1">
+					<div class="box">
+					<img width="85%" src="assets/images/logo.jpg"/ alt="Logo Araujo" class="logo-araujo">
+					<p>
+						A Black Friday Araujo está recheada de promoções imperdíveis! São diversos produtos com descontos mais que especiais. Faça o seu cadastro e receba todas as ofertas com uma hora de antecedência. Não fique de fora dessa!
+					</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+		
+		<div class="container conheca text-center"> 
+			<div class="row">
+				<div class="col-md-12">
+				<p> Conheça todas as categorias que estão participando dessa festa: </p>
+					<ul class="flex-container">
+					  <li class="flex-item"><a href="#">DERMO - COSMÉTICOS </a></li>
+					  <li class="flex-item"><a href="#">MAMÃE E BEBEÊ</a></li>
+					  <li class="flex-item"><a href="#">SAÚDE</a></li>
+					  <li class="flex-item"><a href="#">BELEZA</a></li>
+					  <li class="flex-item"><a href="#">HIGIENE PESSOAL</a></li>
+					  <li class="flex-item"><a href="#">PET SHOP</a></li>
+					  <li class="flex-item"><a href="#">FITNESS</a></li>
+					  <li class="flex-item"><a href="#">MELHOR IDADE</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+</body>
+</html>
